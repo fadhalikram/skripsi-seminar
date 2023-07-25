ba3e1a60816223ab864a3d9fdff7c92f4e389ad9
Ikram <fadhalikram@gmail.com>
2023-07-25 08:00:15 +0700

    Update Dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -7,6 +7,8 @@ use Illuminate\Http\Request;
 // use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use Carbon\Carbon;
+use App\Models\Category;
+
 
 class DashboardController extends Controller
 {
@@ -18,21 +20,67 @@ class DashboardController extends Controller
     public function index(request $request)
     {
         $title = "Dashboard";
+        $categories = Category::get();
+
+        $categoryId = $request->input('category_id');
+        $dateStart = $request->input('date_start');
+        $dateEnd = $request->input('date_end');
+
         // Menghitung jumlah event per periode (1 bulan terakhir)
-        $eventsPerMonth = DB::table('events')
-            ->select(DB::raw('DATE_FORMAT(created_at, "%Y-%m") as month'), DB::raw('count(*) as total'))
+        $queryEventsPerMonth = DB::table('events')
+            ->select(DB::raw('DATE_FORMAT(events.date, "%Y-%m") as month'), DB::raw('count(*) as total'))
+            ->leftJoin('registrations', 'events.id', '=', 'registrations.event_id')
             ->groupBy('month')
             ->orderBy('month', 'desc')
-            ->take(12) // Ambil data 12 bulan terakhir
-            ->get();
-
+            ->take(12); 
+            
         // Menghitung jumlah peserta berdasarkan event
-        $participantsPerEvent = DB::table('events')
-            ->join('registrations', 'events.id', '=', 'registrations.event_id')
-            ->select('events.title', DB::raw('count(registrations.id) as total'))
-            ->groupBy('events.title')
-            ->get();
+        $queryParticipantsPerEvent = DB::table('events')
+            ->select(
+                'events.id', 
+                'events.title', 
+                'events.date', 
+                'events.category_id as category_id',
+                'certificates.word_speaker as speaker',
+                'categories.name as category_name',
+                DB::raw('count(registrations.id) as total')
+            )
+            ->leftJoin('registrations', 'events.id', '=', 'registrations.event_id')
+            ->leftJoin('certificates', 'events.id', '=', 'certificates.event_id')
+            ->leftJoin('categories', 'events.category_id', '=', 'categories.id')
+            ->groupBy(
+                'events.id', 
+                'events.title', 
+                'certificates.word_speaker', 
+                'categories.name', 
+                'events.category_id',
+                'events.date'
+            )
+            ->orderBy('events.date', 'desc');
+        // return $queryParticipantsPerEvent->get();
 
-        return view('pages.dashboard.index', compact('title', 'eventsPerMonth', 'participantsPerEvent'));
+        if($categoryId !== null) {
+            $queryEventsPerMonth->where('events.category_id', $categoryId);
+            $queryParticipantsPerEvent->where('events.category_id', $categoryId);
+        }
+        
+        if ($dateStart !== null && $dateEnd !== null) {
+            $queryEventsPerMonth->where('events.date', ">=", $dateStart)->where('events.date', "<=",$dateEnd);
+            $queryParticipantsPerEvent->where('events.date', ">=", $dateStart)->where('events.date', "<=",$dateEnd);
+        } else {
+            if($dateStart !== null) {
+                $queryEventsPerMonth->where('events.date', $dateStart);
+                $queryParticipantsPerEvent->where('events.date', $dateStart);
+            } else if ($dateEnd !== null) {
+                $queryEventsPerMonth->where('events.date', $dateEnd);
+                $queryParticipantsPerEvent->where('events.date', $dateEnd);
+            } 
+        }
+        
+        $eventsPerMonth = $queryEventsPerMonth->get();
+        $participantsPerEvent = $queryParticipantsPerEvent->get();
+        // return $participantsPerEvent;
+
+        return view('pages.dashboard.index', compact('title', 'eventsPerMonth', 'participantsPerEvent', 'categories'));
     }
 }
--- resources/views/pages/dashboard/index.blade.php
+++ resources/views/pages/dashboard/index.blade.php
@@ -19,7 +19,7 @@
                     <div class="card-header">
                         <div class="row">
                             <div class="col-6">
-                                <h5 class="mb-0">{{ $title }}</h5>
+                                <h5 class="mb-0">Event Summary</h5>
                             </div>
                         </div>
                     </div>
@@ -29,22 +29,102 @@
                                 <p>{{ $message }}</p>
                             </div>
                         @endif
-
-                        <h4>Jumlah Seminar per Bulan</h4>
-                        <ul>
-                            @foreach ($eventsPerMonth as $event)
-                                <li><strong>{{ $event->total }}</strong> seminar pada {{ Carbon::parse($event->month)->isoFormat('MMMM YYYY') }}</li>
-                            @endforeach
-                        </ul>
-
-                        <!-- Tampilkan jumlah peserta berdasarkan event -->
-                        <h4>Jumlah Peserta per Event</h4>
-                        <ul>
-                            @foreach ($participantsPerEvent as $event)
-                                <li><strong>{{ $event->total }} peserta</strong> pada seminar {{ $event->title}}</li>
-                            @endforeach
-                        </ul>
                         
+                        <form action="{{ route('dashboard.index') }}" method="GET" class="row">
+                            <div class="col-3">
+                                <div class="form-group">
+                                    <label for="category_id">Category:</label>
+                                    <select class="form-control" id="category_id" name="category_id">
+                                        <option value="">Select Event</option>
+                                        @foreach ($categories as $category)
+                                            <option value="{{ $category->id }}" {{ request('category_id') == $category->id ? ' selected' : '' }}>{{ $category->name }}</option>
+                                        @endforeach
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="col-3">
+                                <div class="form-group">
+                                    <label for="category_id">Date Start:</label>
+                                    <input type="date" name="date_start" value="{{ request('date_start') }}" class="form-control"/>
+                                </div>
+                            </div>
+                            <div class="col-3">
+                                <div class="form-group">
+                                    <label for="category_id">Date End:</label>
+                                    <input type="date" name="date_end" value="{{ request('date_end') }}" class="form-control"/>
+                                </div>
+                            </div>
+                            <div class="col-3" style="display: flex; align-items: end;">
+                                <button type="submit" class="btn btn-primary">Filter</button>
+                            </div>
+                        </form>
+
+                        <hr />
+
+                        <div>
+                            <table class="table">
+                                <thead>
+                                <tr>
+                                    <th>No</th>
+                                    <th>Category</th>
+                                    <th>Event</th>
+                                    <th>Participant</th>
+                                    <th>Speaker</th>
+                                    <th>Date</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                    @if($participantsPerEvent->count() > 0)
+                                        @foreach($participantsPerEvent as $event)
+                                            <tr>
+                                                <td>{{ $loop->index + 1 }}</td>
+                                                <td>{{ $event->category_name }}</td>
+                                                <td>{{ $event->title }}</td>
+                                                <td>{{ $event->total }}</td>
+                                                <td>{{ $event->speaker }}</td>
+                                                <td>{{ $event->date }}</td>
+                                            </tr>
+                                        @endforeach
+                                    @else
+                                        <tr>
+                                            <td colspan="5" class="text-center text-secondary">Event not found</td>
+                                        </tr>
+                                    @endif
+                                </tbody>
+                            </table>
+                        </div>
+                        
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-12">
+                <div class="card mt-3">
+                    <div class="card-header">
+                        <div class="row">
+                            <div class="col-6">
+                                <h5 class="mb-0">Number of Seminars per Month</h5>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card-body">
+                        <table class="table">
+                            <thead>
+                            <tr>
+                                <th>No</th>
+                                <th>Month</th>
+                                <th>Event Total</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            @foreach($eventsPerMonth as $event)
+                                <tr>
+                                    <td>{{ $loop->index + 1 }}</td>
+                                    <td>{{ Carbon::parse($event->month)->isoFormat('MMMM YYYY') }}</td>
+                                    <td>{{ $event->total }}</td>
+                                </tr>
+                            @endforeach
+                            </tbody>
+                        </table>
                     </div>
                 </div>
             </div>
